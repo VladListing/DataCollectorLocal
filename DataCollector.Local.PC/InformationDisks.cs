c5ab4c12d0eef2e3f1e41ddd8c8bf249ac79ada2
VladListing <chvch@mail.ru>
2017-11-20 22:04:31 +0700

    created DB " DataLocal "

---
+++ DataCollector.Local.PC/InformationDisks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector.Local.PC
+{
+    internal class InformationDisks
+    {
+
+        public void ScanningDisks()
+        {
+            var allDrives = DriveInfo.GetDrives();
+
+            foreach (var d in allDrives)
+            {
+                Console.WriteLine("MachineName: {0}", Environment.MachineName);
+                Console.WriteLine("Drive {0}", d.Name);
+                Console.WriteLine("  Drive type: {0}", d.DriveType);
+                if (d.IsReady == true)
+                {
+                    Console.WriteLine("  Volume label: {0}", d.VolumeLabel);
+                    Console.WriteLine("  File system: {0}", d.DriveFormat);
+                    Console.WriteLine("  Total size of drive:{0:0.##} Mb ",d.TotalSize/ 1048576.0);
+                    Console.WriteLine("  Total available space:{0:0.##} Mb",d.TotalFreeSpace / 1048576.0);
+
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
